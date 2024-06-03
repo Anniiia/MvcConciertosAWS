c77ffce2af42e07151d5ea2ccda283d956f3cd0b
Anniiia <annaortegabueno@gmail.com>
2024-06-03 10:28:20 +0200

    Add project files.

---
+++ MvcConciertosAWS/Controllers/ConciertosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcConciertosAWS.Models;
+using MvcConciertosAWS.Services;
+
+namespace MvcConciertosAWS.Controllers
+{
+    public class ConciertosController : Controller
+    {
+        private ServiceConciertos service;
+
+        public ConciertosController(ServiceConciertos service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Evento> eventos = await this.service.GetEventosAsync();
+            return View(eventos);
+        }
+
+        public async Task<IActionResult> Categorias()
+        {
+            List<Categoria> categorias = await this.service.GetCategoriasAsync();
+
+            return View(categorias);
+        }
+
+        public async Task<IActionResult> EventosCategorias(int id)
+        {
+
+            List<Evento> eventos = await this.service.GetEventosCategoria(id);
+            return View(eventos);
+        }
+
+
+    }
+}
---
+++ MvcConciertosAWS/Services/ServiceConciertos.cs
@@ -0,0 +1,67 @@
+﻿using MvcConciertosAWS.Models;
+using System.Net.Http.Headers;
+
+namespace MvcConciertosAWS.Services
+{
+    public class ServiceConciertos
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue header;
+
+        public ServiceConciertos(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>
+                ("ApiUrl:ApiConciertosAWS");
+            this.header = new MediaTypeWithQualityHeaderValue
+                ("application/json");
+        }
+
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response =
+                    await client.GetAsync(this.UrlApi + request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public async Task<List<Evento>> GetEventosAsync()
+        {
+            string request = "api/Conciertos";
+            List<Evento> data =
+                await this.CallApiAsync<List<Evento>>(request);
+            return data;
+        }
+
+        public async Task<List<Evento>> GetEventosCategoria(int id)
+        {
+            string request = "api/Conciertos/GetEventosCategorias/" + id;
+            List<Evento> data = await this.CallApiAsync<List<Evento>>(request);
+            return data;
+
+
+        }
+
+        public async Task<List<Categoria>> GetCategoriasAsync()
+        {
+            string request = "api/Conciertos/GetCategorias";
+            List<Categoria> data = await this.CallApiAsync<List<Categoria>>(request);
+            return data;
+        }
+
+
+
+
+    }
+}
